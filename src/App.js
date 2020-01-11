0ba8b9c076a696c2686bb6bb6ca27f0e148fe110
QuangMinh <quangminh.lenguyen.si@gmail.com>
2020-01-12 00:20:59 +0100

    feature/step1: update title app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NeoChart from "./neo/NeoChart";
 const App = () => (
     <div className="App">
         <h1>Botify Nasa</h1>
+        <h4 style={{fontStyle: "italic"}}>(developed by Quang Minh)</h4>
         <div>
             <NeoChart/>
         </div>
